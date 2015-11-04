9326b0b3ee58b8944e4e3816a92838a520d0c05a
Pugachev Dmitry <pugachevdmitry67@gmail.com>
2015-11-04 13:11:27 +0300

    BufferStateFactory was added

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,7 @@ package com.acme.edu;
 
 import com.acme.edu.print.BufferPrinter;
 import com.acme.edu.print.ConsolePrinter;
-import com.acme.edu.state.BufferState;
-import com.acme.edu.state.DefaultBufferState;
-import com.acme.edu.state.IntBufferState;
-import com.acme.edu.state.StringBufferState;
+import com.acme.edu.state.*;
 
 public class Logger {
     // format strings for print()
@@ -20,36 +17,27 @@ public class Logger {
 
 
 
-    private BufferPrinter bufferPrinter = new ConsolePrinter();
+    private BufferPrinter bufferPrinter;
     private BufferState bufferState;
+    private BufferStateFactory bufferStateFactory;
+
+    public Logger() {
+        bufferPrinter = new ConsolePrinter();
+        bufferStateFactory = new BufferStateFactory(bufferPrinter);
+    }
 
     public void log(int message) {
-        if(bufferState == null) {
-            bufferState = new IntBufferState(bufferPrinter);
-        } else if(bufferState.getState() != BufferState.State.INT) {
-            bufferState.printBuffer();
-            bufferState = new IntBufferState(bufferPrinter);
-        }
+        bufferState = bufferStateFactory.switchToIntState(bufferState);
         bufferState.pushMessageToBuffer(Integer.toString(message), PRIMITIVE_FORMAT);
     }
 
     public void log(String message) {
-        if(bufferState == null) {
-            bufferState = new StringBufferState(bufferPrinter);
-        }else if(bufferState.getState() != BufferState.State.STRING) {
-            bufferState.printBuffer();
-            bufferState = new StringBufferState(bufferPrinter);
-        }
+        bufferState = bufferStateFactory.switchToStringState(bufferState);
         bufferState.pushMessageToBuffer(message, STRING_FORMAT);
     }
 
     public void log(char message) {
-        if(bufferState == null) {
-            bufferState = new DefaultBufferState(bufferPrinter);
-        } else if (bufferState.getState() != BufferState.State.DEFAULT) {
-            bufferState.printBuffer();
-            bufferState = new DefaultBufferState(bufferPrinter);
-        }
+        bufferState = bufferStateFactory.switchToDefaultState(bufferState);
         bufferState.pushMessageToBuffer(Character.toString(message), CHAR_FORMAT);
     }
 
