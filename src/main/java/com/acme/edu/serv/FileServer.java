24eb55296164ea24b00d47781141ceeeb820c1d0
SuperDuperPugach <pugachevdmitry67@gmail.com>
2015-11-09 17:55:39 +0400

    Buffer manager on server was added

--- src/main/java/com/acme/edu/serv/FileServer.java
+++ src/main/java/com/acme/edu/serv/FileServer.java
@@ -5,6 +5,8 @@ package com.acme.edu.serv;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Сервер, записывающий принимаемые сообщения в лог файл
@@ -12,11 +14,15 @@ import java.net.Socket;
 public class FileServer {
     private static final String END_LOG = "END_LOG";
     private static final String ERROR_LOG = "Can't write logs to file on server!";
+    private static final String FILE_NAME = "log.dat";
+    private static final int BUFFER_SIZE = 50;
+
     private ServerSocket serverSocket;
     private Socket client;
     private BufferedReader fromClient;
-    private static final String FILE_NAME = "log.dat";
-    private int count; //счетчик вызовов метода принт
+    private List<String> buffer; //буффер получаемых сообщений
+
+   // private int count; //счетчик вызовов метода принт
     /**
      * Конструктор, принимающий в качестве параметра номер порта. Сервер помещается в
      * режим ожидания соединения с клиентом
@@ -27,12 +33,14 @@ public class FileServer {
     public FileServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         client = serverSocket.accept();
-        count = 1;
+        buffer = new ArrayList<>();
+       // count = 1;
     }
 
     /**
      * Метод, в которм сервер устанавливает потоки соединения с клиентом и производится
-     * запись информации от клинета в файл. При возниконовении ошибки записи в файл
+     * запись информации от клинета в файл при накоплении достаточного количества сообщений.
+     * При возниконовении ошибки записи в файл
      * клиенту передается оповещение
      * @throws IOException - бросается, если невозможно установить входной или выходной поток с клиентом
      */
@@ -48,12 +56,13 @@ public class FileServer {
             while (true) {
                 readline = fromClient.readLine();
                 if (readline.equals(END_LOG)) {
-                    toFile.flush();
+                    writeBufferToFile(toFile);
                     break;
                 }
-                toFile.write(readline + System.lineSeparator());
-                if ((count % 50) == 0) toFile.flush();
-                count++;
+                buffer.add(readline);
+                if(buffer.size() == BUFFER_SIZE) {
+                    writeBufferToFile(toFile);
+                }
             }
         } catch (IOException e) {
             //здесь нужно передать клиенту сообщение об ошибке записи логов в файл
@@ -63,6 +72,14 @@ public class FileServer {
                 toClient.write(ERROR_LOG + System.lineSeparator());
             }
         }
+    }
+
+    private void writeBufferToFile(BufferedWriter toFile) throws IOException {
+        for(String s : buffer) {
+            toFile.write(s + System.lineSeparator());
+        }
+        toFile.flush();
+        buffer.clear();
     }
 
 
